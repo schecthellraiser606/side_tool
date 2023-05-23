c5af9e3a1a8ffc095ee5e2d0e81667529068cea7
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-05-23 23:16:35 +0900

    mapping

--- sqlmap/go/http2grpc.go
+++ sqlmap/go/http2grpc.go
@@ -5,17 +5,22 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 )
 
 type Request struct {
 	ID string `json:"id"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 const (
 	hostname     = "10.129.93.227:50051" // ホスト名とポートを指定
 	serviceName  = "SimpleApp"    // サービス名を指定
 	methodName   = "getInfo"       // メソッド名を指定
-	tokenHeader = "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidGVzdHUiLCJleHAiOjE2ODQ3NzI0MTd9.DfVyzGde6IjLfxgin65F7aAaplqMxJ0jteB6CQtVaVo"
+	tokenHeader = "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYWFhYSIsImV4cCI6MTY4NDg2MTExOX0.8dyUoaB562MTJk-wznf39-HiaVjmlwhGVtY7YRzNb2k"
 	httpPort     = ":8051"           // HTTPのポート番号を指定
 )
 
@@ -34,13 +39,32 @@ func main() {
 
 		// HTTPリクエストをgRPCリクエストに変換して転送する
 		cmd := exec.Command("grpcurl", "--plaintext", "--rpc-header", tokenHeader, "-d", string(reqBody), hostname, serviceName+"."+methodName)
-		cmd.Stdout = w
-
-		err = cmd.Run()
+		output, err := cmd.Output()
 		if err != nil {
 			log.Printf("Error executing grpcurl: %v", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
 		}
+
+		resp := string(output)
+		if strings.HasPrefix(resp, "ERROR:") {
+			// エラーレスポンスの場合はそのまま返す
+			http.Error(w, resp, http.StatusInternalServerError)
+			return
+		}
+
+		// レスポンスをパースしてJSONの中身を取得
+		var response Response
+		err = json.Unmarshal([]byte(resp), &response)
+		if err != nil {
+			log.Printf("Error unmarshaling response body: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// JSONの中身をレスポンスとして返す
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 	})
 
 	log.Println("HTTP to gRPC proxy server started on", httpPort)
